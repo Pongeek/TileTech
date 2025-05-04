340ef93e5e4cb39475e0bfab577ef29051c35f27
Pongeek <maximpim95@gmail.com>
2025-05-04 14:36:08 -0400

    feat(services): move back button arrow to left and flip direction

--- src/components/sections/Services.tsx
+++ src/components/sections/Services.tsx
@@ -269,10 +269,10 @@ const Services: React.FC = () => {
                       whileHover={{ x: -5 }}
                       whileTap={{ scale: 0.98 }}
                     >
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 ml-1 transform rtl:rotate-180">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
-                      </svg>
                       <span>חזרה לרשימת השירותים</span>
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 mr-1">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18 " />
+                      </svg>
                     </motion.button>
                   </div>
                   
